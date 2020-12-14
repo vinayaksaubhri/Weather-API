e43a4a021c6d4b0e92642ef5c91b47cd9905ce21
Vinayak <vinayaksaubhri@gmail.com>
2020-12-14 14:37:10 +0530

    first commit

---
+++ node.js
@@ -0,0 +1,76 @@
+window.addEventListener('load',()=>{
+  const api_key= "6274503762ffc89f4efbecb5791450e7";
+  let lat;
+  let long;
+  navigator.geolocation.getCurrentPosition(position => {
+      // console.log(position);
+      long =position.coords.longitude;
+      lat=position.coords.latitude;
+      // console.log(long,lat);
+
+
+      const proxy='https://cors-anywhere.herokuapp.com/'
+      const api=`${proxy}api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api_key}&units=metric`;
+      // console.log(api);
+
+      
+    fetch(api).then(weather => {
+      return weather.json()
+    }).then(displayResults);
+  })
+})
+
+const api = {
+    key: "6274503762ffc89f4efbecb5791450e7",
+    base: "https://api.openweathermap.org/data/2.5/"
+  }
+  
+
+const searchbox = document.querySelector('.search-box');
+searchbox.addEventListener('keypress', setQuery);
+function setQuery(evt) {
+  if (evt.keyCode == 13) {
+    getResults(searchbox.value);
+  }
+}
+
+
+function getResults (query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then(weather => {
+      return weather.json();
+    }).then(displayResults);
+}
+
+function displayResults (weather) {
+  // console.log(weather);
+  let city = document.querySelector('.location .city');
+  city.innerText = `${weather.name}, ${weather.sys.country}`;
+
+  let now = new Date();
+  let date = document.querySelector('.location .date');
+  date.innerText = dateBuilder(now);
+
+  let temp = document.querySelector('.current .temp');
+  temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
+
+  let weather_el = document.querySelector('.current .weather');
+  weather_el.innerText = weather.weather[0].main;
+
+  let hilow = document.querySelector('.hi-low');
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(weather.main.temp_max)}°c`;
+}
+
+function dateBuilder (d) {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
+
+
